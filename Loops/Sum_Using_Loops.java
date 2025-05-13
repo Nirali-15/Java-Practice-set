edab3377a3fb184f08e2fe305d64c0a4648a25e8
Nirali-15 <niralibhanvadiya73@gmail.com>
2025-05-13 11:31:42 +0530

    Loops

--- Loops/Sum_Using_Loops.java
+++ Loops/Sum_Using_Loops.java
@@ -5,10 +5,9 @@ public class Sum_Using_Loops {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum = sum + i;
-
+        for (int first = 1; first <= n; first++) {
+            sum += first;
         }
-        System.out.println(sum);
+        System.out.println("Sum of " + n + " natural numbers is: " + sum + ".");
     }
 }
